c4be7c4991930fdd2de9ae291f1cad09c2e693f6
Gagasr10 <dragan.stojilkovic85@gmail.com>
2026-04-09 07:30:44 +0200

    feat: add RetryListener to automatically apply retry logic to all tests- Implemented IAnnotationTransformer- Automatically binds RetryAnalyzer to every @Test method- Registered via @Listeners in BaseTest

---
+++ src/test/java/tests/RetryListener.java
@@ -0,0 +1,18 @@
+package tests;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+/**
+ * Binds the RetryAnalyzer to every test method automatically.
+ * This listener is registered in BaseTest via @Listeners.
+ */
+public class RetryListener implements IAnnotationTransformer {
+    @Override
+    public void transform(ITestAnnotation annotation, Class testClass,
+                          Constructor testConstructor, Method testMethod) {
+        annotation.setRetryAnalyzer(RetryAnalyzer.class);
+    }
